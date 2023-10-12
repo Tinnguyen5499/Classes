28eb16e1a3fb1f0c74e2ac4959cea1ef5da7081f
Tin Nguyen <64116909+Tinnguyen5499@users.noreply.github.com>
2023-10-12 11:56:00 -0700

    update

--- FALL 2023/MAE 144/hw1.m
+++ FALL 2023/MAE 144/hw1.m
@@ -1,3 +1,4 @@
+close all; clear all; clc
 %%Problem 1
 %Part a
 
@@ -7,12 +8,16 @@ b= RR_poly([1 0 -29 0 100]);
 
 f= RR_poly([1 8 23 36 45]);
 
-[x,y,r,t]= RR_diophantine(a,b,f);
+[x,y,r,t]= RR_diophantine(a,b,f)
 
 test= a*x+b*y;
 
 %Part b
+% The controller in 1a is improper
 
+f2= RR_poly([1 8 23 36 45 ]);
+
+[x2,y2,r2,t2]= RR_diophantine(a,b,f2)
 
 %%Problem 2
 
