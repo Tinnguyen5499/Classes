1f314c244bf30d75bf8eca3267fb96057c57707f
Tin Nguyen <64116909+Tinnguyen5499@users.noreply.github.com>
2023-10-12 11:52:58 -0700

    part 1a

--- FALL 2023/MAE 144/hw1.m
+++ FALL 2023/MAE 144/hw1.m
@@ -1,9 +1,18 @@
+%%Problem 1
+%Part a
+
+a= RR_poly([1 0 -45 0 369 0 -324]);
+
+b= RR_poly([1 0 -29 0 100]);
+
+f= RR_poly([1 8 23 36 45]);
+
+[x,y,r,t]= RR_diophantine(a,b,f);
+
+test= a*x+b*y;
+
+%Part b
 
 
-a= RR_poly([1 0 -45 0 369 0 -324])
+%%Problem 2
 
-b= RR_poly([1 0 -29 0 100])
-
-f= RR_poly([1 8 23 36 45])
-
-[x,y,r,t]= RR_diophantine(a,b,f)
