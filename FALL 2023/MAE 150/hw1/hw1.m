259c24365d7210c033599663447f9a21140f8240
Tin Nguyen <64116909+Tinnguyen5499@users.noreply.github.com>
2023-10-28 21:09:20 -0700

    good

--- FALL 2023/MAE 150/hw1/hw1.m
+++ FALL 2023/MAE 150/hw1/hw1.m
@@ -14,14 +14,29 @@ A= [ 3 3 4 4 5 5 6 6 3;
      2 4 4 3 3 4 4 2 2;
      1 1 1 1 1 1 1 1 1];
 
-
 figure (1)
-
 xlim([-10 10])
 ylim([-10 10])
 xline(0)
 yline(0)
-box on; grid on
-
+box on; grid on;
 hold on
 
+plotf(A)
+
+%moving A to 6,3
+
+A1=Tt(6-3,3-2)*A;
+plotf(A1)
+
+A2=Tfy*A1;
+
+plotf(A2)
+
+A3=Tt(6,-3)*Ts(0.6,0.6)*Tt(-6,3)*A2;
+
+plotf(A3)
+
+A4=Tt(7,-2)*Tr(315)*Tt(-7,2)*A3;
+
+plotf(A4)
